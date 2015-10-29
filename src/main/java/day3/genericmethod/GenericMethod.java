559bb49f3aba713a4c6e2d638ba6d5a825c9d761
student <student@M16023>
2015-10-29 12:25:36 +0900

    generic method

--- src/main/java/day3/genericmethod/GenericMethod.java
+++ src/main/java/day3/genericmethod/GenericMethod.java
@@ -1,63 +1,53 @@
-
 package day3.genericmethod;
 
-class AAA
-{
-	public String methodA()
-	{
+class AAA {
+	public String methodA() {
 		return "Class AAA 의 메서드 호출";
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return "Class AAA 의 인스턴스";
 	}
 }
 
-class BBB
-{
-	public String methodB()
-	{
+class BBB {
+	public String methodB() {
 		return "Class BBB 의 메서드 호출";
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return "Class BBB 의 인스턴스";
 	}
 }
 
-class GenericMethodCall
-{
-	public <T> void gMethod(T inst)
-	{
-		String whatMethod ;
-		
+class GenericMethodCall {
+	public <T> void gMethod(T inst) {
+		String whatMethod;
+
 		System.out.println(inst);
-		
-		if(inst instanceof AAA){
-			whatMethod = ((AAA) inst).methodA() ;
+
+		if (inst instanceof AAA) {
+			whatMethod = ((AAA) inst).methodA();
+		} else {
+			whatMethod = ((BBB) inst).methodB();
 		}
-		else{
-			whatMethod = ((BBB)inst).methodB() ;
-		}
-		
+
 		System.out.println(whatMethod);
 	}
-	
+
 }
 
 public class GenericMethod {
 
 	public static void main(String[] args) {
-		
-		AAA aaa=new AAA();
-		BBB bbb=new BBB();
-		
-		GenericMethodCall shower=new GenericMethodCall();
+
+		AAA aaa = new AAA();
+		BBB bbb = new BBB();
+
+		GenericMethodCall shower = new GenericMethodCall();
 		shower.gMethod(aaa);
-//		shower.<AAA>gMethod(aaa);
-		shower.<BBB>gMethod(bbb);
+		// shower.<AAA>gMethod(aaa);
+		shower.<BBB> gMethod(bbb);
 
 	}
 }
