cef2bf6131157158d28ed633c2895dc9f7bba25c
student <student@M16023>
2015-10-30 15:21:30 +0900

    my filter

--- src/main/java/day4/mystream/MyFilterTest.java
+++ src/main/java/day4/mystream/MyFilterTest.java
@@ -1,4 +1,3 @@
-
 package day4.mystream;
 
 import java.io.*;
@@ -7,14 +6,17 @@ import day4.charstream6.MyFilter;
 
 public class MyFilterTest {
 
-  public static void main(String[] args) throws IOException {
-    int c;
+	public static void main(String[] args) throws IOException {
+		int c;
 
-InputStream in = new MyFilter(new BufferedInputStream(new FileInputStream("src/test/java/day4/mystream/MyFilterTest/secondfilterdata.txt")));
+		InputStream in = new MyFilter(
+				new BufferedInputStream(
+						new FileInputStream(
+								"src/test/java/day4/mystream/MyFilterTest/secondfilterdata.txt")));
 
-    while((c = in.read( )) >= 0) {
-	System.out.print((char)c);
-    }
-	in.close();
-    }
+		while ((c = in.read()) >= 0) {
+			System.out.print((char) c);
+		}
+		in.close();
+	}
 }
